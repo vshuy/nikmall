3ed8f605b00d61316990a739f45df20967d45f84
vshuy <vshbmt@gmail.com>
2022-02-18 21:26:18 +0700

    update GUI2

--- src/views/bill/BillEdit.vue
+++ src/views/bill/BillEdit.vue
@@ -1,55 +1,64 @@
 <template>
-  <div class="container w-50">
-    <h2>UPDATE STATUS THIS BILL</h2>
-    <h3>Customer name: {{ bill.bill.user.name }}</h3>
-    <h4>Email address: {{ bill.bill.user.email }}</h4>
-    <h4>Phone: {{ bill.bill.user.phone_number }}</h4>
-    <h4>Date order: {{ bill.bill.created_at }}</h4>
-    <h5>
-      Total cost $: {{ bill.bill.total }} || {{ bill.detailBill.length }} items
-    </h5>
-    <span>Update this order</span>
-    <select :value="bill.bill.paid_status" @input="setBillStatusForAnBill">
-      <option
-        v-for="(option, index) in bill_status"
-        v-bind:value="option.id"
-        :key="index"
-      >
-        {{ option.status }}
-      </option>
-    </select>
-    <div class="row">
-      <div
-        v-for="(item, index) in bill.detailBill"
-        :key="index"
-        class="col-sm-12 border-bottom mt-1"
-      >
-        <div class="row">
-          <div class="col-sm-3">
-            <img
-              v-bind:src="item.product.link_thumbnail"
-              alt="n"
-              width="80px"
-              height="80px"
-            />
-          </div>
-          <div class="col-sm-9">
-            <div style="display: inline-block; font-weight: 900">
-              {{ item.product.name }}
+  <div class="container">
+    <div class="row"><Header></Header></div>
+    <div class="container w-50">
+      <h2>UPDATE STATUS THIS BILL</h2>
+      <h3>Customer name: {{ bill.bill.user.name }}</h3>
+      <h4>Email address: {{ bill.bill.user.email }}</h4>
+      <h4>Phone: {{ bill.bill.user.phone_number }}</h4>
+      <h4>Date order: {{ bill.bill.created_at }}</h4>
+      <h5>
+        Total cost $: {{ bill.bill.total }} ||
+        {{ bill.detailBill.length }} items
+      </h5>
+      <span>Update this order</span>
+      <select :value="bill.bill.paid_status" @input="setBillStatusForAnBill">
+        <option
+          v-for="(option, index) in bill_status"
+          v-bind:value="option.id"
+          :key="index"
+        >
+          {{ option.status }}
+        </option>
+      </select>
+      <div class="row">
+        <div
+          v-for="(item, index) in bill.detailBill"
+          :key="index"
+          class="col-sm-12 border-bottom mt-1"
+        >
+          <div class="row">
+            <div class="col-sm-3">
+              <img
+                v-bind:src="item.product.link_thumbnail"
+                alt="n"
+                width="80px"
+                height="80px"
+              />
             </div>
-            <p>$ {{ item.product.cost }}</p>
-            <p>{{ item.amounts }} items</p>
+            <div class="col-sm-9">
+              <div style="display: inline-block; font-weight: 900">
+                {{ item.product.name }}
+              </div>
+              <p>$ {{ item.product.cost }}</p>
+              <p>{{ item.amounts }} items</p>
+            </div>
           </div>
         </div>
       </div>
+      <button
+        type="button"
+        class="btn btn-success mx-auto mt-2"
+        v-on:click="update()"
+      >
+        UPDATE STATUS
+      </button>
     </div>
-    <button type="button" class="btn btn-success mx-auto mt-2" v-on:click="update()">
-      UPDATE STATUS
-    </button>
   </div>
 </template>
 <script>
 import { mapActions, mapState, mapMutations } from 'vuex';
+import Header from './../../components/Header.vue';
 
 export default {
   name: 'BillEdit',
@@ -63,7 +72,7 @@ export default {
       initStore: 'initStore',
       update: 'update',
     }),
-     ...mapMutations('bill', {
+    ...mapMutations('bill', {
       setBillStatusForAnBill: 'setBillStatusForAnBill',
     }),
   },
@@ -72,6 +81,9 @@ export default {
       bill: (state) => state.bill,
       bill_status: (state) => state.bill_status,
     }),
+  },
+  components: {
+    Header,
   },
   mounted() {
     this.show(this.$route.params.id);
--- src/views/historyOrder/OrderShow.vue
+++ src/views/historyOrder/OrderShow.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="container w-50">
-    <h2>DETAIL THIS ORDER</h2>
-    <h3>Customer name: {{ bill.bill.user.name }}</h3>
-    <h4>Email address: {{ bill.bill.user.email }}</h4>
-    <h5>
-      Total cost $: {{ bill.bill.total }} || {{ bill.detailBill.length }} items
-    </h5>
-    <h6>Status: {{ bill.bill.bill_status.status }}</h6>
-    <h6>Address: {{ bill.bill.address.name_address }}</h6>
-    <div class="row">
-      <div
-        v-for="(item, index) in bill.detailBill"
-        :key="index"
-        class="col-sm-12 border-bottom mt-1"
-      >
-        <div class="row">
-          <div class="col-sm-3">
-            <img
-              v-bind:src="item.product.link_thumbnail"
-              alt="n"
-              width="80px"
-              height="80px"
-            />
-          </div>
-          <div class="col-sm-9">
-            <div style="display: inline-block; font-weight: 900">
-              {{ item.product.name }}
+  <div class="container">
+    <div class="row"><Header></Header></div>
+    <div class="container w-50">
+      <h2>DETAIL THIS ORDER</h2>
+      <h3>Customer name: {{ bill.bill.user.name }}</h3>
+      <h4>Email address: {{ bill.bill.user.email }}</h4>
+      <h5>
+        Total cost $: {{ bill.bill.total }} ||
+        {{ bill.detailBill.length }} items
+      </h5>
+      <h6>Status: {{ bill.bill.bill_status.status }}</h6>
+      <h6>Address: {{ bill.bill.address.name_address }}</h6>
+      <div class="row">
+        <div
+          v-for="(item, index) in bill.detailBill"
+          :key="index"
+          class="col-sm-12 border-bottom mt-1"
+        >
+          <div class="row">
+            <div class="col-sm-3">
+              <img
+                v-bind:src="item.product.link_thumbnail"
+                alt="n"
+                width="80px"
+                height="80px"
+              />
             </div>
-            <p>$ {{ item.product.cost }}</p>
-            <p>{{ item.amounts }} items</p>
+            <div class="col-sm-9">
+              <div style="display: inline-block; font-weight: 900">
+                {{ item.product.name }}
+              </div>
+              <p>$ {{ item.product.cost }}</p>
+              <p>{{ item.amounts }} items</p>
+            </div>
           </div>
         </div>
       </div>
@@ -37,7 +41,7 @@
 </template>
 <script>
 import { mapActions, mapState } from 'vuex';
-
+import Header from './../../components/Header.vue';
 export default {
   name: 'OrderShow',
   metaInfo: {
@@ -53,6 +57,9 @@ export default {
     ...mapState('bill', {
       bill: (state) => state.bill,
     }),
+  },
+  components: {
+    Header,
   },
   mounted() {
     this.show(this.$route.params.id);
